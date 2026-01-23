19057dfa91af717d4b13cdce06d10d9b17a38e6f
Khalil Gabsi <khalilgabssi8@gmail.com>
2026-01-23 23:23:17 +0100

    Create enumerator.sh

---
+++ enumerator.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+
+if [ $# -ne 2 ]
+then
+echo "missing arguments"
+exit 1
+fi
+for word in $`cat $1`
+do
+response=$(curl -I $2/$word 2>/dev/null | grep HTTP |cut -d " " -f2)
+if [ "$response" -le 399 ]
+then
+echo "$word : $response"
+
+fi
+done
+
